af062998a424d6f8daf9e5729e02057a88a0bf14
sqp0627 <1551936315@qq.com>
2017-10-25 21:13:02 +0800

    header component edit

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -27,30 +27,40 @@
     <div class="background">
       <img :src="seller.avatar" width="100%" height="100%">
     </div>
-    <div v-show="detailShow" class="detail">
-      <div class="detail-wrapper clearfix">
-        <div class="detail-main">
-          <h1 class="name">{{seller.name}}</h1>
-          <div class="star-wrapper">
-            <star :size="48" :score="seller.score"></star>
+    <transition name="fade">
+      <div v-show="detailShow" class="detail" transition="fade">
+        <div class="detail-wrapper clearfix">
+          <div class="detail-main">
+            <h1 class="name">{{seller.name}}</h1>
+            <div class="star-wrapper">
+              <star :size="48" :score="seller.score"></star>
+            </div>
+            <div class="title">
+              <div class="line"></div>
+              <div class="text">优惠信息</div>
+              <div class="line"></div>
+            </div>
+            <ul v-if="seller.supports" class="supports">
+              <li class="support-item" v-for="(item,index) in seller.supports">
+                <span class="icon" :class="classMap[seller.supports[index].type]"></span>
+                <span class="text">{{seller.supports[index].description}}</span>
+              </li>
+            </ul>
+            <div class="title">
+              <div class="line"></div>
+              <div class="text">商家公告</div>
+              <div class="line"></div>
+            </div>
+            <div class="bulletin">
+              <p class="content">{{seller.bulletin}}</p>
+            </div>
           </div>
-          <div class="title">
-            <div class="line"></div>
-            <div class="text">优惠信息</div>
-            <div class="line"></div>
-          </div>
-          <ul v-if="seller.supports" class="supports">
-            <li class="support-item" v-for="(item,index) in seller.supports">
-              <span class="icon" :class="classMap[seller.supports[index].type]"></span>
-              <span class="text">{{seller.supports[index].description}}</span>
-            </li>
-          </ul>
+        </div>
+        <div class="detail-close" @click="hideDetail">
+          <i class="icon-close"></i>
         </div>
       </div>
-      <div class="detail-close">
-        <i class="icon-close"></i>
-      </div>
-    </div>
+    </transition>
   </div>
 </template>
 <script>
@@ -69,6 +79,9 @@
     methods: {
       showDetail: function () {
         this.detailShow = true
+      },
+      hideDetail: function () {
+        this.detailShow = false
       }
     },
     created() {
@@ -210,7 +223,11 @@
       overflow: auto
       z-index: 1
       background: rgba(7,17,27,0.8)
-      .detail-wrapper
+      &.fade-enter-active, &.fade-leave-active
+        transition: all 0.5s
+      &.fade-enter, &.fade-leave-active
+        opacity: 0
+    .detail-wrapper
         width:100%
         min-height: 100%
         .detail-main
@@ -240,7 +257,7 @@
               font-size: 14px
           .supports
             width:80%
-            margin:0 auto
+            margin: 0 auto
             .support-item
               padding: 0 12px
               margin-bottom: 12px
@@ -267,10 +284,18 @@
                 &.special
                   bg-image('special_2')
               .text
-                line-height: 12px
+                line-height: 16px
                 font-size: 12px
                 font-weight: 200
                 color: rgb(255,255,255)
+          .bulletin
+            width: 80%
+            margin:0 auto
+            .content
+              padding:0 12px
+              line-height: 24px
+              font-size: 12px
+              font-weight: 200
       .detail-close
         position: relative;
         width: 32px
